070bda18c62c6a78946b536456433e3cef4b769c
eagle1024 <45815353+eagle1024@users.noreply.github.com>
2022-06-25 14:55:09 +0800

    modify

--- src/test/java/com/devcloud/demo/DealthMaxTime.java
+++ src/test/java/com/devcloud/demo/DealthMaxTime.java
@@ -11,6 +11,7 @@ public class DealthMaxTime {
         int[] count = new int[101];
         int max = 0;
         int min = 0;
+        int len = 0;
         for (int i = 0; i < count.length; i++) {
             int iSum = 0;
             for (Pair pair : list) {
